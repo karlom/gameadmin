5efa5fa60eb13c9570e9daa8de6f48cb00613a20
karlom <karlomgg@gmail.com>
2016-04-09 23:42:57 +0800

    删除太旧的文件
    之前的文件太老，现在重新上传一个完整的系统

--- public/index.php
+++
@@ -1,2 +0,0 @@
-<?php
-header('Location:/module/index.php');
--- public/module/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-include_once '../../protected/config/config.php';
-include_once SYSDIR_ADMIN_INCLUDE.'/global.php';
-render('index.html');
